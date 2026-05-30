304eaf083f66c9d0e110119818180f53719a22fc
PlotChat <levietanh1234567@gmail.com>
2026-05-30 13:06:48 +0700

    feat: make spring boot targets the whole backend folder instead of just application/

--- Backend/src/main/java/swp391/group6/application/TreeshopmanagingsystemApplication.java
+++ Backend/src/main/java/swp391/group6/application/TreeshopmanagingsystemApplication.java
@@ -2,8 +2,10 @@ package swp391.group6.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "swp391.group6")
+@EntityScan(basePackages = "swp391.group6.model")
 public class TreeshopmanagingsystemApplication {
 
 	public static void main(String[] args) {
