fe1deb9540afa74f33c917fdee221b3369ed4e43
lmd100 <ducbup157@gmail.com>
2026-06-01 09:31:13 +0700

    fix: fixed the create users api so now it properly works with hashed password

--- Backend/src/main/java/swp391/group6/application/TreeshopmanagingsystemApplication.java
+++ Backend/src/main/java/swp391/group6/application/TreeshopmanagingsystemApplication.java
@@ -1,10 +1,17 @@
-package swp391.group6;
+package swp391.group6.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "swp391.group6")
+@EnableJpaRepositories(basePackages = "swp391.group6.repository")
+@EntityScan(basePackages = "swp391.group6.model")
 public class TreeshopmanagingsystemApplication {
+    static {
+        System.setProperty("user.timezone", "Asia/Ho_Chi_Minh");
+    }
     public static void main(String[] args) {
         SpringApplication.run(TreeshopmanagingsystemApplication.class, args);
     }
