da19ac1822309616d6473b519dbcb8c72383e088
PlotChat <levietanh1234567@gmail.com>
2026-06-04 15:32:37 +0700

    feat: add proxy to vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		proxy: {
+		// Whenever React tries to fetch '/api/...', Vite will forward it to port 8080
+		'/api': {
+			target: 'http://localhost:8080',
+			changeOrigin: true,
+		}
+		}
+	}
 });
