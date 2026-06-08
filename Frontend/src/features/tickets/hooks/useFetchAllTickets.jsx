4ccfbd37bae2af7191a245e87e53c97047dd47d9
PlotChat <levietanh1234567@gmail.com>
2026-06-08 13:14:09 +0700

    feat: changes for Ticket Dashboard frontend and hooks that it uses
    - Refactor for UI's single responsibility, separating filter components from the main dashboard component
    - Minor UI changes
    - Refactor for functions' single responsibility, separating hooks and functions to different hooks/ files

--- Frontend/src/features/tickets/hooks/useFetchAllTickets.jsx
+++ Frontend/src/features/tickets/hooks/useFetchAllTickets.jsx
@@ -1,26 +1,41 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { fetchAllTickets } from "../data/ticketApi.js";
 
 const useFetchAllTickets = (initialTickets = []) => {
 	const [fetchedTickets, setFetchedTickets] = useState(initialTickets);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const [fetchAllTicketsError, setFetchAllTicketsError] = useState(null);
 
-	const executeFetchAllTickets = useCallback(async (ticketFilter, ticketPiority, ticketSort) => {
-        setIsLoading(true);
-        setError(null)
+	const executeFetchAllTickets = async (
+		ticketFilter,
+		ticketPiority,
+		ticketSort,
+	) => {
+		setIsLoading(true);
+		setFetchAllTicketsError(null);
 
-        try{
-            const data = await fetchAllTickets(ticketFilter, ticketPiority, ticketSort);
-            setFetchedTickets(data);
-        } catch(error){
-            setError(error);
-        } finally{
-            setIsLoading(false);
-        }
-	}, []);
+		try {
+			const data = await fetchAllTickets(
+				ticketFilter,
+				ticketPiority,
+				ticketSort,
+			);
+			setFetchedTickets(data);
+		} catch {
+			const errorMessage = "Đã xảy ra lỗi khi tải dữ liệu."; // Hardcoded message
 
-	return { isLoading, error, fetchedTickets, executeFetchAllTickets };
+			setFetchAllTicketsError(errorMessage);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return {
+		isLoading,
+		fetchAllTicketsError,
+		fetchedTickets,
+		executeFetchAllTickets,
+	};
 };
 
 export default useFetchAllTickets;
