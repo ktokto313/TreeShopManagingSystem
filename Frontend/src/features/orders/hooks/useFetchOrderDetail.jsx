ae09cd319af485820997cba2e86ea95651f13c8b
ktokto313 <kto3132006@gmail.com>
2026-06-12 23:30:40 +0700

    cleanup import

--- Frontend/src/features/orders/hooks/useFetchOrderDetail.jsx
+++ Frontend/src/features/orders/hooks/useFetchOrderDetail.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function useFetchOrderDetail() {
     const [orderDetail, setOrderDetail] = useState(null);
