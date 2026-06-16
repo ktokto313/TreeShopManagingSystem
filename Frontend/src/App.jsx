8a9fa6a02d9d59c9f52a2f45120ca418c21478c8
Aiden <levietanh1234567@gmail.com>
2026-06-16 07:36:05 +0700

    feat: add loading screen page component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,6 +17,8 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 import StaffAuthentication from "./pages/StaffAuthentication";
 import UserManagement from "./pages/UserManagement";
 import TicketManagement from "./pages/TicketManagement";
+import HomePage from "./pages/HomePage";
+import LoadingScreen from "./pages/LoadingScreen";
 
 function ProtectedRoute({ element, roles = [] }) {
 	const { user, isLoading } = useAuth();
