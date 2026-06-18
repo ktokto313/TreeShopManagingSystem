e3d8f8c9032eda948f80567f20acf3c8cc48571c
Aiden <levietanh1234567@gmail.com>
2026-06-19 00:43:50 +0700

    feat,chore: enhance home hero section, minor bug fix in profile page

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 ﻿import { Link } from "react-router-dom";
 import { Container } from "../components/global/Container";
 import { cn } from "../utils/cn";
+import bg from "../assets/images/home-bg.jpg";
+import { MdOutlineTipsAndUpdates } from "react-icons/md";
 
 function ActionLink({ className, to, children, variant = "primary" }) {
 	const baseClass = cn(
@@ -30,21 +32,29 @@ function ActionLink({ className, to, children, variant = "primary" }) {
 export default function HomePage() {
 	return (
 		<main>
-			<section className="bg-linear-to-br from-green-300 via-white to-green-200">
+			<section className="relative overflow-hidden">
 				<Container className="py-16 lg:py-24">
-					<div className="space-y-6">
+					<img
+						src={bg}
+						className="absolute object-cover inset-0 h-full w-full -z-20"
+					></img>
+
+					{/* Color filter for the section */}
+					{/* <div className="absolute opacity-80 inset-0 bg-linear-to-br from-green-300/80 via-white/70 to-green-200/80 -z-10 backdrop-blur-sm"></div> */}
+
+					<div className="relative bg-linear-to-b rounded-3xl from-green-300/80 via-50% via-white/70 to-transparent p-5 sm:p-8 lg:p-12">
 						<div className="space-y-4">
-							<h1 className="max-w-2xl text-4xl font-semibold tracking-tight text-green-800 sm:text-5xl">
+							<h1 className="max-w-2xl text-2xl sm:text-3xl md:text-4xl font-semibold tracking-tight text-green-800">
 								Khám phá cây xanh cho không gian sống gần gũi và dễ chăm hơn
 							</h1>
-							<p className="max-w-2xl text-lg text-green-700">
+							<p className="max-w-2xl text-base sm:text-lg md:text-xl text-green-700">
 								Một cửa vào nhẹ nhàng cho khách yêu cây: xem gợi ý, tìm cảm hứng
 								và bước vào catalog để chọn cây phù hợp với nhà ở, bàn làm việc
 								hay góc thư giãn.
 							</p>
 						</div>
 
-						<div className="flex flex-wrap gap-3">
+						<div className="flex flex-wrap gap-3 mt-8">
 							<ActionLink
 								to="/catalog"
 								variant="primary"
@@ -61,10 +71,13 @@ export default function HomePage() {
 							</ActionLink>
 						</div>
 
-						<p className="max-w-2xl text-sm text-green-700">
-							Gợi ý nhanh: cây để bàn, cây lọc không khí, chậu và phụ kiện cho
-							góc xanh của bạn.
-						</p>
+						<div className="flex gap-1 items-center text-xs sm:text-sm mt-7 text-white/90 bg-green-500/60 rounded-xl p-2 px-4 w-max max-w-full">
+							<MdOutlineTipsAndUpdates className="text-base md:text-lg"></MdOutlineTipsAndUpdates>
+							<p>
+								Gợi ý nhanh: cây để bàn, cây lọc không khí, chậu và phụ kiện cho
+								góc xanh của bạn.
+							</p>
+						</div>
 					</div>
 				</Container>
 			</section>
