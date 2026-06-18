a89aed4dafc3b504befb095a93c31cf4611c6c1d
Aiden <levietanh1234567@gmail.com>
2026-06-18 12:58:49 +0700

    feat: fix and change auth feature pages

--- Frontend/src/features/auth/pages/RegisterPage.jsx
+++ Frontend/src/features/auth/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useRegister } from '../hooks/useRegister'
+import registerImg from "../assets/images/registerImg.jpg"
 
 const EyeIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -14,6 +15,7 @@ const EyeOffIcon = () => (
       <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
     </svg>
 )
+
 export default function RegisterPage() {
   const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState(false)
@@ -45,12 +47,27 @@ export default function RegisterPage() {
   }
 
   return (
-      <div className="min-h-[calc(100vh-4rem)] flex">
+      <div className="flex flex-col-reverse md:flex-row-reverse md:min-h-screen min-h-[90vh]">
+        <div className="bg-blue-500 text-white relative flex-1 flex items-center justify-center min-h-[350px] md:min-h-0">
+          <div className="absolute inset-0">
+            <img src={registerImg} className="object-cover w-full h-full" alt="" />
+          </div>
+          <div className="bg-blue-300/90 relative z-10 p-10 m-6 md:m-10 max-w-sm w-full md:w-auto">
+            <h2 className="text-2xl font-bold uppercase mb-4">Login</h2>
+            <p className="mb-8 text-left text-pretty w-[80%] text-lg font-semibold">
+              Already have an account? Log in!
+            </p>
+            <button
+                onClick={() => navigate('/login')}
+                className="px-6 py-2.5 text-xs font-bold uppercase border border-white hover:bg-white hover:text-blue-600 transition cursor-pointer"
+            >
+              Login
+            </button>
+          </div>
+        </div>
 
-        {/* LEFT - FORM */}
-        <div className="w-[60%] bg-bg-base flex items-center justify-center px-16 py-12">
+        <div className="flex flex-col items-center justify-center px-16 md:px-12 lg:px-0 pt-12 pb-12 md:pt-0 md:pb-20 md:flex-1 bg-amber-200/30">
           <div className="w-full max-w-md">
-
             <h1 className="text-2xl font-bold tracking-wider uppercase mb-8 text-black">
               Register
             </h1>
@@ -63,7 +80,6 @@ export default function RegisterPage() {
 
             {!otpSent ? (
                 <form onSubmit={handleSubmit}>
-
                   <div className="mb-5">
                     <label className="block text-sm mb-1.5 text-black">Full name *</label>
                     <input
@@ -72,7 +88,7 @@ export default function RegisterPage() {
                         onChange={handleChange}
                         required
                         disabled={loading}
-                        className="w-full border border-white-subtle px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
+                        className="w-full border-[0.01rem] border-green-600 focus:border-[0.12rem] transition-property:border px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
                     />
                   </div>
 
@@ -85,7 +101,7 @@ export default function RegisterPage() {
                         onChange={handleChange}
                         required
                         disabled={loading}
-                        className="w-full border border-white-subtle px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
+                        className="w-full border-[0.01rem] border-green-600 focus:border-[0.12rem] px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
                     />
                   </div>
 
@@ -99,7 +115,7 @@ export default function RegisterPage() {
                           onChange={handleChange}
                           required
                           disabled={loading}
-                          className="w-full border border-white-subtle px-3 py-2.5 pr-10 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
+                          className="w-full border-[0.01rem] border-green-600 focus:border-[0.12rem]px-3 py-2.5 pr-10 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
                       />
                       <button
                           type="button"
@@ -127,7 +143,7 @@ export default function RegisterPage() {
                           className={`w-full border px-3 py-2.5 pr-10 text-sm focus:outline-none disabled:opacity-50 ${
                               confirmError
                                   ? 'border-red-400 focus:border-red-400'
-                                  : 'border-white-subtle focus:border-green-500'
+                                  : 'border-[0.01rem] border-green-600 focus:border-[0.12rem]'
                           }`}
                       />
                       <button
@@ -147,7 +163,7 @@ export default function RegisterPage() {
                   <button
                       type="submit"
                       disabled={loading}
-                      className="px-8 py-3 text-xs font-bold uppercase bg-green-800 text-white hover:bg-green-700 disabled:opacity-50 transition cursor-pointer"
+                      className="w-full px-8 py-3 text-xs font-bold uppercase bg-green-600 text-white hover:bg-green-500 disabled:opacity-50 transition cursor-pointer"
                   >
                     {loading ? 'Sending OTP...' : 'Register'}
                   </button>
@@ -165,7 +181,7 @@ export default function RegisterPage() {
                         onChange={(e) => setOtp(e.target.value)}
                         maxLength={6}
                         disabled={loading}
-                        className="w-full border border-white-subtle px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
+                        className="w-full border-[0.01rem] border-green-600 focus:border-[0.12rem] px-3 py-2.5 text-sm focus:outline-none focus:border-green-500 disabled:opacity-50"
                     />
                   </div>
 
@@ -193,20 +209,6 @@ export default function RegisterPage() {
                 </div>
             )}
           </div>
-        </div>
-
-        {/* RIGHT - CTA */}
-        <div className="w-[40%] bg-blue-500 flex flex-col items-center justify-center p-12 text-white">
-          <h2 className="text-xl font-bold uppercase mb-4">Login</h2>
-          <p className="mb-8 text-center text-sm">
-            Already have an account? Log in!
-          </p>
-          <button
-              onClick={() => navigate('/login')}
-              className="px-6 py-2.5 text-xs font-bold uppercase border border-white hover:bg-white hover:text-blue-500 transition cursor-pointer"
-          >
-            Login
-          </button>
         </div>
       </div>
   )
