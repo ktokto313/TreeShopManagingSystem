8a788720cfb263f4269c93117fbc68451a5e6b6c
Aiden <levietanh1234567@gmail.com>
2026-06-18 23:27:07 +0700

    feat,chore: ticket flow filter and ticket detail - add extra filter for ticket management page - hide ticket detail status change option for dropdown - fix imports for ticket detail page

--- Frontend/src/pages/TicketManagement.jsx
+++ Frontend/src/pages/TicketManagement.jsx
@@ -9,12 +9,12 @@ const TicketManagement = () => {
 	return (
 		<div className="flex flex-row gap-2 w-full p-4">
 			<TicketDashboardFilterBoard
-				classNames="hidden lg:flex flex-1 top-4"
+				classNames="hidden md:flex flex-2 max-w-70 top-4"
 				dashboardState={dashboardState}
 			/>
 			<TicketDashboard
 				className={cn(
-					"border-2 border-border mx-auto w-full rounded-2xl min-h-180 flex flex-col flex-6",
+					"border-2 border-border mx-auto w-full rounded-2xl min-h-180 flex flex-col flex-5",
 				)}
 				dashboardState={dashboardState}
 			/>
