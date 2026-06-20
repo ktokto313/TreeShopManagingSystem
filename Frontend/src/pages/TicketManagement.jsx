5d5f07f49e4a91728acb479fa935960a74e80f27
Aiden <levietanh1234567@gmail.com>
2026-06-21 00:27:18 +0700

    feat,style: ticket detail now processes update ticket status differently, and minor changes
    # Conflicts:
    #	Backend/src/main/java/swp391/group6/model/User.java
    #	Frontend/src/features/tickets/components/TicketDashboard.jsx
    #	Frontend/src/features/tickets/components/TicketDetail.jsx
    #	Frontend/src/features/tickets/components/ticket-dashboard/TicketDashboardFilter.jsx
    #	Frontend/src/features/tickets/data/ticketSelectList.js
    #	Frontend/src/features/tickets/hooks/useTicketDetail.jsx
    #	Frontend/src/pages/TicketManagement.jsx

--- Frontend/src/pages/TicketManagement.jsx
+++ Frontend/src/pages/TicketManagement.jsx
@@ -1,5 +1,5 @@
-import TicketDashboard from "../features/tickets/components/ticket-dashboard/TicketDashboard";
-import { TicketDashboardFilterBoard } from "../features/tickets/components/ticket-dashboard/TicketDashboardFilter";
+import TicketDashboard from "../features/tickets/components/TicketDashboard";
+import { TicketDashboardFilterBoard } from "../features/tickets/components/TicketDashboardFilter";
 import { useTicketDashboard } from "../features/tickets/hooks/useTicketDashboard";
 import { cn } from "../utils/cn";
 
@@ -9,12 +9,12 @@ const TicketManagement = () => {
 	return (
 		<div className="flex flex-row gap-2 w-full p-4">
 			<TicketDashboardFilterBoard
-				classNames="hidden md:flex flex-2 max-w-70 top-4"
+				classNames="hidden lg:flex flex-1 top-4"
 				dashboardState={dashboardState}
 			/>
 			<TicketDashboard
 				className={cn(
-					"border-2 border-border mx-auto w-full rounded-2xl min-h-180 flex flex-col flex-5",
+					"border-2 border-border mx-auto w-full rounded-2xl min-h-180 flex flex-col flex-6",
 				)}
 				dashboardState={dashboardState}
 			/>
