1ec0da69ccabccec7a000e61b0870b6662b4eb60
Aiden <levietanh1234567@gmail.com>
2026-06-28 12:52:13 +0700

    feat: add pagination for reviews

--- Backend/src/main/java/swp391/group6/repository/ReviewRepository.java
+++ Backend/src/main/java/swp391/group6/repository/ReviewRepository.java
@@ -1,5 +1,7 @@
 package swp391.group6.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import swp391.group6.model.Review;
 
@@ -7,5 +9,6 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByOrderDetail_Product_Id(Long id);
+    Page<Review> findByOrderDetail_Product_Id(Long id, Pageable pageable);
     boolean existsByOrderDetail_Order_IdAndOrderDetail_Product_Id(Long orderId, Long productId);
 }
