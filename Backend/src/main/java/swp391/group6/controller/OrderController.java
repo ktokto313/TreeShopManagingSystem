34a409b735a33d2365ad96c73eec0ab696e740d6
lmd100 <ducbup157@gmail.com>
2026-06-28 09:30:48 +0700

    fix: update OrderController authorization - MANAGER only for PUT /{id}, add CUSTOMER for /{id}/status

--- Backend/src/main/java/swp391/group6/controller/OrderController.java
+++ Backend/src/main/java/swp391/group6/controller/OrderController.java
@@ -73,6 +73,7 @@ public class OrderController {
     }
 
     @PutMapping("{id}")
+    @PreAuthorize("hasAnyRole('MANAGER')")
     public ResponseEntity<Void> changeOrder(HttpServletRequest request, @PathVariable long id, @RequestBody OrderDTO order) {
         LoginResponse loginResponse = JWTUtil.getUser(request);
         try {
@@ -87,7 +88,7 @@ public class OrderController {
     }
 
     @PutMapping("{id}/status")
-    @PreAuthorize("hasAnyRole('MANAGER', 'SHIPPER')")
+    @PreAuthorize("hasAnyRole('MANAGER', 'SHIPPER', 'CUSTOMER')")
     public ResponseEntity<Void> changeOrderStatus(HttpServletRequest request, @PathVariable long id, @RequestParam(name = "status") OrderStatus orderStatus) {
         LoginResponse loginResponse = JWTUtil.getUser(request);
         if (!orderService.changeOrderStatus(id, orderStatus, loginResponse)) {
