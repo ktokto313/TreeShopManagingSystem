46f89d08c75964e44969fc7ca1b39a35c950e668
Hung Dao <phucdao285@gmail.com>
2026-07-16 21:01:32 +0700

    Update notification feature

--- Backend/src/main/java/swp391/group6/model/NotificationType.java
+++ Backend/src/main/java/swp391/group6/model/NotificationType.java
@@ -9,27 +9,25 @@
 package swp391.group6.model;
 
 public enum NotificationType {
-    // Customer
+
+    //CUSTOMER
     ORDER_CONFIRMATION,
     ORDER_STATUS_UPDATE,
     DELIVERY_COMPLETED,
     SUPPORT_TICKET_RESOLVED,
     BLOG_STATUS_UPDATE,
-    OTP_VERIFICATION,
+    SUPPORT_TICKET_UPDATE,
 
-    // Manager
+    //MANAGER
     NEW_ORDER_ALERT,
-    LOW_STOCK_ALERT,
     BLOG_PENDING_APPROVAL,
 
-    // Shipper
+    //SHIPPER
     DELIVERY_ASSIGNMENT,
 
-    // Customer Support
-    DELIVERY_EXCEPTION,
+    //SUPPORT
     NEW_SUPPORT_REQUEST,
 
-    // System Admin
-    SECURITY_ALERT,
-    SYSTEM_CONFIG_CHANGE
+    //SYSTEM
+    DELIVERY_EXCEPTION
 }
