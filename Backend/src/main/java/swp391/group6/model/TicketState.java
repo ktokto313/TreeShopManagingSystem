89a6a97dd0dd0e380a4b4e96a671818a276faaba
Aiden <levietanh1234567@gmail.com>
2026-07-20 15:56:04 +0700

    chore: fix comments for creators

--- Backend/src/main/java/swp391/group6/model/TicketState.java
+++ Backend/src/main/java/swp391/group6/model/TicketState.java
@@ -6,14 +6,7 @@
  * Last Change Author: AnhLV
  * Last Change Date: 2026-07-03
  */
-/*
- * Author: ktokto313
- * Created Date: 2026-05-29
- * Name: TicketState.java
- * Description: 
- * Last Change Author: ktokto313
- * Last Change Date: 2026-05-29
- */
+
 package swp391.group6.model;
 
 public enum TicketState {
