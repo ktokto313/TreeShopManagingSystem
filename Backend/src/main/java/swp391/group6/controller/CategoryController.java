7abfa2c853064b17002cd01c93bf201753a41caa
minhlthe200133 <mmuvn876@gmail.com>
2026-07-20 14:05:33 +0700

    chore: remove additional duplicate comments

--- Backend/src/main/java/swp391/group6/controller/CategoryController.java
+++ Backend/src/main/java/swp391/group6/controller/CategoryController.java
@@ -1,9 +1,4 @@
 /*
- * Created By: MinhLTHE200133
- * Created At: 2026-05-30
- * Last Modified: 2026-07-03
- */
-/*
  * Author: minhlthe200133
  * Created Date: 2026-05-30
  * Name: CategoryController.java
